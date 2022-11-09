94aa24d96a7967129afb5e0e6efa2173c29f57c4
ThanhdatOSD <100662958+ThanhdatOSD@users.noreply.github.com>
2022-11-09 17:44:34 +0700

    complete register form ui

--- SocialWebApp/WebUI/ClientApp/src/app/pages/auth/components/register/register.component.ts
+++ SocialWebApp/WebUI/ClientApp/src/app/pages/auth/components/register/register.component.ts
@@ -1,33 +1,49 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {TuiCountryIsoCode} from '@taiga-ui/i18n';
+import {
+  emailValidator,
+  nameValidator,
+  passwordValidator,
+  phoneValidator,
+  userNameValidator
+} from "../../validators/register-validator";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  encapsulation:ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class RegisterComponent implements OnInit {
   gender = [{name: `Male`}, {name: `Female`}];
+  countries: TuiCountryIsoCode[] = Object.values(TuiCountryIsoCode);
+  countryIsoCode = TuiCountryIsoCode.VN;
+  readonly emailValidator = Validators.email;
+  sendingState:boolean = false;
+
 
   readonly registerForm = new FormGroup({
-    firstNameValue: new FormControl(``, [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]')]),
-    lastNameValue: new FormControl(``, Validators.required),
-    userNameValue: new FormControl(``, Validators.required),
-    passwordValue: new FormControl(``, Validators.required),
-    emailValue: new FormControl(``, Validators.email),
-    phoneValue: new FormControl(``, Validators.minLength(12)),
+    firstNameValue: new FormControl(``, nameValidator("First name")),
+    lastNameValue: new FormControl(``, nameValidator("Last name")),
+    userNameValue: new FormControl(``, userNameValidator),
+    passwordValue: new FormControl(``, passwordValidator),
+    emailValue: new FormControl(``, emailValidator),
+    phoneValue: new FormControl(``, phoneValidator),
     genderValue: new FormControl(this.gender[0]),
-    conditionValue: new FormControl(false)
+    conditionValue: new FormControl(true, Validators.requiredTrue)
   });
-  countries: TuiCountryIsoCode[] = Object.values(TuiCountryIsoCode);
-
-  countryIsoCode = TuiCountryIsoCode.VN;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+ ngOnInit() {
+ }
+
+  onRegisterFormSubmitted(){
+    this.sendingState = true;
+    console.log(this.registerForm.value);
   }
 }
---
+++ SocialWebApp/WebUI/ClientApp/src/app/pages/auth/validators/register-validator.ts
@@ -0,0 +1,64 @@
+import {AbstractControl, ValidatorFn, Validators} from "@angular/forms";
+
+const nameChars = /^[A-Za-z][a-zA-Z ]*/
+const userNameChars = /^[A-Za-z][a-zA-Z0-9]*/
+const emailChars = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+const passwordChars = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+/*
+* Password rule:
+* Minimum eight characters, at least one letter, one uppercase letter and one number
+*/
+
+export function nameValidator(fieldName: string): ValidatorFn {
+  return (field: AbstractControl): Validators | null => {
+    if (!field.value)
+      return {other: `Please enter your ${fieldName}`};
+    if (!nameChars.test(field.value))
+      return {other: `${fieldName} is invalid. Please enter another value`};
+    if (field.value.length < 2)
+      return {other: `${fieldName} must be at least 2 characters long. Please use another one`};
+    if (field.value.length > 50)
+      return {other: `${fieldName} is limited on the number of 50 characters. Please use another one`};
+    return null;
+  }
+}
+
+export function userNameValidator(field: AbstractControl): Validators | null {
+  if (!field.value)
+    return {other: `Please enter your user name`};
+  if (!userNameChars.test(field.value))
+    return {other: `The user name is invalid. Please use another one which not start with a number and not contain special characters, spaces or symbols`};
+  if (field.value.length < 5)
+    return {other: `The user name must be at least 5 characters long. Please use another one`};
+  if (field.value.length > 32)
+    return {other: `The user name is limited on the number of 32 characters. Please use another one`};
+  return null;
+}
+
+export function passwordValidator(field: AbstractControl): Validators | null {
+  if (!field.value)
+    return {other: `Please enter your password`};
+  if (!passwordChars.test(field.value))
+    return {other: `Please enter your password with at least 1 uppercase and 1 number `};
+  if (field.value.length < 8)
+    return {other: `The password must be at least 8 characters long. Please use another one`};
+  if (field.value.length > 32)
+    return {other: `The password is limited on the number of 32 characters. Please use another one`};
+  return null;
+}
+
+export function emailValidator(field: AbstractControl): Validators | null {
+  if (!field.value)
+    return {other: `Please enter a valid email address`};
+  if(!emailChars.test(field.value))
+    return {other: `The email you entered is not a valid email address`};
+  return null;
+}
+
+export function phoneValidator(field: AbstractControl): Validators | null {
+  if(!field.value)
+    return null;
+  if(field.value.length < 11 || field.value.length > 15)
+    return {other: `The phone number must be between 8 and 15 digits`};
+  return null;
+}
